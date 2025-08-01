5ad8aeca211020b7a12d8e0db9b0c92deefdcef4
Trinanjan2003 <trinanjankumarbaul>
2025-08-01 10:17:20 +0530

    Final Push of the Project

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Custom Models : - 
-from .models import Account 
+from .models import Account , UserProfile
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 
 # Register your models here.
 
@@ -17,4 +18,13 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+    
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+    list_display_links = ('thumbnail', 'user')
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- store/views.py
+++ store/views.py
@@ -15,6 +15,7 @@ from carts.views import _cart_id
 from .forms import ReviewForm
 from django.contrib import messages
 from orders.models import OrderProduct
+from .models import ProductGallery
 
 
 
@@ -67,14 +68,14 @@ def product_detail(request, category_slug, product_slug):
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
     # Get the product gallery
-    # product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
 
     context = {
         'single_product': single_product,
         'in_cart'       : in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
-        # 'product_gallery': product_gallery,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
